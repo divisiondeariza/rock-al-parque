f1fc5147eaa78d7486dd7b370b9e46fd025151ed
Emmanuel Ariza <emmanuel@emmanuel.emmanuel>
2017-05-14 12:31:53 -0500

    Update 2017-05-14T17:31:50.815Z

--- view1/view1.js
+++ view1/view1.js
@@ -14,18 +14,67 @@ angular.module('myApp.view1', ['ngRoute'])
 	$http.get("data/bandsByCountry.json")
 			.success(function(data, status, headers, config){
 				$scope.bands = data;
+
+				$scope.bands.forEach(function(band){
+					band.isPlaying = false;
+					band.currentTrack = 0;
+				})
 			} );
 
 	d3.csv("data/countriesCodes.csv",function(data){
-		console.log(data[0])
 		$scope.countriesCodes = data[0];
 	});
 
+
+
 	$scope.audio = null;
-	$scope.playAudio = function(element){
-		var url = element.band.top_tracks[0].audio;
+
+	$scope.forwardAndPlay=function(band){
+		band.currentTrack = (band.currentTrack + 1) % band.top_tracks.length;
+		$scope.playAudio(band);
+	}
+
+
+	$scope.rewindAndPlay=function(band){
+		band.currentTrack = (band.currentTrack - 1 + band.top_tracks.length) % band.top_tracks.length;
+		$scope.playAudio(band);
+	}
+
+	$scope.toggleAudio = function(band){
+		if(band.isPlaying){
+			$scope.audio.pause();
+		}
+		else{
+			if($scope.audio) $scope.audio.pause();
+			$scope.playAudio(band);
+		}
+	}
+
+	$scope.playAudio = function(band){
+		band.isPlaying = true;
+		var track = band.top_tracks[band.currentTrack]
+		var url = track.audio;
 		if($scope.audio) $scope.audio.pause();
 		$scope.audio = new Audio(url)
+		$scope.audio.addEventListener('ended', function(){
+			$scope.$apply(function () {
+				band.isPlaying = false;
+				band.currentTrack = (band.currentTrack + 1) % band.top_tracks.length;
+				}
+			);
+		});
+		$scope.audio.addEventListener('pause', function(){
+			$scope.$apply(function () {
+				band.isPlaying = false;
+				}
+			);
+		});
+		$scope.audio.addEventListener('play', function(){
+			$scope.$apply(function () {
+				band.isPlaying = true;
+				}
+			);
+		});
 		$scope.audio.play();
 	}
 
